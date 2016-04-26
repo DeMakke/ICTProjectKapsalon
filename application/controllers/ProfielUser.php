96aaeabdd236ce7e600d5cfdd4a214e36a197973
nielsbekkers <Niels@MacBook-Pro-van-Niels-2.local>
2016-04-26 15:41:57 +0200

    Security lek opgelost

--- application/controllers/ProfielUser.php
+++ application/controllers/ProfielUser.php
@@ -8,6 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ProfielUser extends CI_Controller{
     function __construct() {
         parent::__construct();
+        
+        if(empty($this->session->userdata('id_user'))) {
+            $this->session->set_flashdata('flash_data', 'You don\'t have access!');
+            redirect('login');
+        }
     }
     public function index(){
         $this->load->view("profielUser");

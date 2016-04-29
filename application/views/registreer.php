ad91ca49c6d213c8b3468f55f441f720bd828d56
nielsbekkers <Niels@mbp-van-niels-2.khlim.local>
2016-04-29 11:25:54 +0200

    layout register page

--- application/views/registreer.php
+++ application/views/registreer.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<meta content="text/html; charset=utf-8" />
-		<title>Login Systeem</title>
+		<title>Registreer Jezelf!</title>
                 <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>/assets/css/style.css">
 	</head>
 	<body>
@@ -14,6 +14,7 @@
         
 		<center>
 			<div id="login-form">
+                            <!--<h1><img src="https://cdn0.iconfinder.com/data/icons/academics-linear-black/2048/Register-512.png" width="80"height="80"/>Gelieve te registreren</h1>-->
 				<form method="post">
 					<table align="center" width="30%" border="0">
 						<tr>
@@ -32,7 +33,7 @@
 							<td><button type="submit" name="btn-signup">Registreer</button></td>
 						</tr>
 						<tr>
-							<td><a href="login">Home Pagina</a></td>
+                                                    <td><a href="login" id="linkhomepagina"><img src="https://image.freepik.com/free-icon/home_318-42210.png"width="30" height="30"/>&nbsp;Home Pagina</a></td>
 						</tr>
 					</table>
 				</form>

59a306470a646b301ad04cba5fb578da9c0c0afd
janyoulton <j.youlton@gmail.com>
2016-05-14 15:31:12 +0200

    afspraak model

--- application/models/afspraakUser_model.php
+++ application/models/afspraakUser_model.php
@@ -1,5 +1,4 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,36 +11,28 @@ class afspraakUser_model extends CI_Model
         $this->load->database();
     }
     
-    public function toonBehandeling(){
-        $query = $this->db->get('tblbehandeling');
-        $behandeling = '';
-        
-        foreach($query->result_array() as $row){
-            $behandeling .= '<option value="'.$row['BehandelingID'].'">'.$row['Type'].'</option>';
-        }
-        return $behandeling;
+    public function toonBehandeling($id){
+        return $this->db->get_where('tblbehandeling');
     }
     
-    public function toonPersoneel(){
-        $query = $this->db->get('tblpersoneel');
-        $personeel = '';
-        
-        foreach($query->result_array() as $row){
-            $personeel .= '<option value="'.$row['KapsterID'].'">'.$row['Voornaam'].'</option>';
-        }
-        return $personeel;
+    public function getTijd($id){
+        return $this->db->get_where('tblafspraak');
     }
     
-    public function toonMaand(){
-        $months = array();
-        $currentMonth = (int)date('m');
-        $html = '';
-        
-        for ($x = $currentMonth; $x < $currentMonth + 12; $x++) {
-            $months[$x] = date('F', mktime(0, 0, 0, $x));
-            $html .= '<option value="'.$x.'">'.$months[$x].'</option>';
-        }
-        
-        return $html;
+    public function insertAfspraak($data){
+        return $this->db->insert('tblafspraak',$data);
     }
+    
+//    public function toonMaand(){
+//        $months = array();
+//        $currentMonth = (int)date('m');
+//        $html = '';
+//        
+//        for ($x = $currentMonth; $x < $currentMonth + 12; $x++) {
+//            $months[$x] = date('F', mktime(0, 0, 0, $x));
+//            $html .= '<option value="'.$x.'">'.$months[$x].'</option>';
+//        }
+//        return $html;
+//    }
+//    
 }

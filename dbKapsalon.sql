f5cd12f9258d76ec94f914c3d96411f9ab7a4447
KerimBayramoglu <kerim.bayramoglu@student.ucll.be>
2016-05-15 09:32:57 +0200

    deze database zou volledig in orde moeten zijn, kijk of de relaties er zijn en klik ook op tabel login en dan vanboven ergens 'handelingen' ->middelste kolom ergens, zou auto_increment moeten staan, check ook of da op 2 staat voor zowel login als tblklant (y)

--- dbKapsalon.sql
+++ dbKapsalon.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Gegenereerd op: 02 mei 2016 om 19:09
--- Serverversie: 5.6.26
--- PHP-versie: 5.6.12
+-- Host: 127.0.0.1
+-- Gegenereerd op: 15 mei 2016 om 09:29
+-- Serverversie: 10.1.13-MariaDB
+-- PHP-versie: 7.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,83 +17,72 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbKapsalon`
+-- Database: `dbkapsalon`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `Login`
+-- Tabelstructuur voor tabel `login`
 --
 
-CREATE TABLE IF NOT EXISTS `Login` (
+CREATE TABLE `login` (
   `id_user` int(5) NOT NULL,
   `username` varchar(25) NOT NULL,
   `user_email` varchar(35) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden geëxporteerd voor tabel `Login`
---
-
-INSERT INTO `Login` (`id_user`, `username`, `user_email`, `password`) VALUES
-(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(11, 'Niels', 'niels_bekkers_9@hotmail.com', '9b9824df65f716b71db9f51299346088'),
-(12, 'test', 'test', '098f6bcd4621d373cade4e832627b4f6'),
-(42, 'hallo', 'hallo', '598d4c200461b81522a3328565c25f7c'),
-(44, 'test1', 'test1', '5a105e8b9d40e1329780d62ea2265d8a'),
-(45, 'test3', 'test3', '8ad8757baa8564dc136c1e07507f4a98');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `tblAfspraak`
---
-
-CREATE TABLE IF NOT EXISTS `tblAfspraak` (
-  `voornaam` varchar(30) NOT NULL,
-  `KapsterID` int(5) NOT NULL,
-  `BehandelingID` int(5) NOT NULL,
-  `AfspraakID` int(10) NOT NULL,
-  `Datum` date DEFAULT NULL,
-  `Tijd` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden geëxporteerd voor tabel `tblAfspraak`
+-- Gegevens worden geëxporteerd voor tabel `login`
 --
 
-INSERT INTO `tblAfspraak` (`voornaam`, `KapsterID`, `BehandelingID`, `AfspraakID`, `Datum`, `Tijd`) VALUES
-('1', 1, 1, 1, '2016-05-19', '16:30:00');
+INSERT INTO `login` (`id_user`, `username`, `user_email`, `password`) VALUES
+(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblBehandeling`
+-- Tabelstructuur voor tabel `tblafspraak`
 --
 
-CREATE TABLE IF NOT EXISTS `tblBehandeling` (
+CREATE TABLE `tblafspraak` (
+  `AfspraakID` int(5) NOT NULL,
+  `id_user` int(5) NOT NULL,
+  `KapsterID` int(5) NOT NULL,
+  `Tijd` date NOT NULL,
+  `BehandelingID` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tblbehandeling`
+--
+
+CREATE TABLE `tblbehandeling` (
   `BehandelingID` int(5) NOT NULL,
   `Type` text NOT NULL,
   `Tijd` time NOT NULL,
   `Prijs` varchar(5) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden geëxporteerd voor tabel `tblBehandeling`
+-- Gegevens worden geëxporteerd voor tabel `tblbehandeling`
 --
 
-INSERT INTO `tblBehandeling` (`BehandelingID`, `Type`, `Tijd`, `Prijs`) VALUES
-(1, 'Knippen', '00:15:00', '15');
+INSERT INTO `tblbehandeling` (`BehandelingID`, `Type`, `Tijd`, `Prijs`) VALUES
+(1, 'Knippen', '00:15:00', '15'),
+(2, 'Föhnen', '00:15:00', ''),
+(3, 'Brushen', '00:15:00', '10');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblKlant`
+-- Tabelstructuur voor tabel `tblklant`
 --
 
-CREATE TABLE IF NOT EXISTS `tblKlant` (
+CREATE TABLE `tblklant` (
   `id_user` int(5) NOT NULL,
   `username` varchar(30) NOT NULL,
   `Achternaam` text NOT NULL,
@@ -101,24 +90,15 @@ CREATE TABLE IF NOT EXISTS `tblKlant` (
   `Telefoonnr` int(9) NOT NULL,
   `user_email` varchar(35) NOT NULL,
   `Geslacht` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden geëxporteerd voor tabel `tblKlant`
---
-
-INSERT INTO `tblKlant` (`id_user`, `username`, `Achternaam`, `Voornaam`, `Telefoonnr`, `user_email`, `Geslacht`) VALUES
-(1, 'Jos', 'Voorbeeld', 'Dorpstraat', 11234567, 'jos@example.com', 'Man'),
-(12, 'test', 'test', 'test', 457463748, 'testblablabla', 'man'),
-(45, 'test3', 'test3', 'test3', 2147483647, 'testblablabla', 'man');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tblPersoneel`
+-- Tabelstructuur voor tabel `tblpersoneel`
 --
 
-CREATE TABLE IF NOT EXISTS `tblPersoneel` (
+CREATE TABLE `tblpersoneel` (
   `KapsterID` int(5) NOT NULL,
   `Voornaam` text NOT NULL,
   `Achternaam` text NOT NULL,
@@ -131,48 +111,53 @@ CREATE TABLE IF NOT EXISTS `tblPersoneel` (
   `Email` text NOT NULL,
   `Geboortedatum` date NOT NULL,
   `Geslacht` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden geëxporteerd voor tabel `tblPersoneel`
+-- Gegevens worden geëxporteerd voor tabel `tblpersoneel`
 --
 
-INSERT INTO `tblPersoneel` (`KapsterID`, `Voornaam`, `Achternaam`, `Straat`, `Nr`, `Plaats`, `Postcode`, `Telefoonnr`, `Gsmnr`, `Email`, `Geboortedatum`, `Geslacht`) VALUES
-(1, 'Tom', 'Herten', 'Hertenstraat', 3, 'Hasselt', 3500, '011356783', '0474569345', 'tomherten@gmail.com', '2015-05-05', 'Man');
+INSERT INTO `tblpersoneel` (`KapsterID`, `Voornaam`, `Achternaam`, `Straat`, `Nr`, `Plaats`, `Postcode`, `Telefoonnr`, `Gsmnr`, `Email`, `Geboortedatum`, `Geslacht`) VALUES
+(1, 'Kerim', 'Bayramoglu', 'Lupienenstraat', 33, 'genk', 3600, '123456789', '987654321', 'kerim@example.com', '2016-02-19', 'Man');
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexen voor tabel `Login`
+-- Indexen voor tabel `login`
 --
-ALTER TABLE `Login`
+ALTER TABLE `login`
   ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `email` (`user_email`);
+  ADD UNIQUE KEY `email` (`user_email`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
--- Indexen voor tabel `tblAfspraak`
+-- Indexen voor tabel `tblafspraak`
 --
-ALTER TABLE `tblAfspraak`
-  ADD PRIMARY KEY (`AfspraakID`);
+ALTER TABLE `tblafspraak`
+  ADD PRIMARY KEY (`AfspraakID`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `KapsterID` (`KapsterID`,`BehandelingID`),
+  ADD KEY `BehandelingID` (`BehandelingID`);
 
 --
--- Indexen voor tabel `tblBehandeling`
+-- Indexen voor tabel `tblbehandeling`
 --
-ALTER TABLE `tblBehandeling`
+ALTER TABLE `tblbehandeling`
   ADD PRIMARY KEY (`BehandelingID`);
 
 --
--- Indexen voor tabel `tblKlant`
+-- Indexen voor tabel `tblklant`
 --
-ALTER TABLE `tblKlant`
-  ADD PRIMARY KEY (`id_user`,`username`);
+ALTER TABLE `tblklant`
+  ADD PRIMARY KEY (`id_user`,`username`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
 
 --
--- Indexen voor tabel `tblPersoneel`
+-- Indexen voor tabel `tblpersoneel`
 --
-ALTER TABLE `tblPersoneel`
+ALTER TABLE `tblpersoneel`
   ADD PRIMARY KEY (`KapsterID`);
 
 --
@@ -180,25 +165,48 @@ ALTER TABLE `tblPersoneel`
 --
 
 --
--- AUTO_INCREMENT voor een tabel `Login`
+-- AUTO_INCREMENT voor een tabel `login`
 --
-ALTER TABLE `Login`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
+ALTER TABLE `login`
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT voor een tabel `tblBehandeling`
+-- AUTO_INCREMENT voor een tabel `tblafspraak`
 --
-ALTER TABLE `tblBehandeling`
-  MODIFY `BehandelingID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `tblafspraak`
+  MODIFY `AfspraakID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT voor een tabel `tblKlant`
+-- AUTO_INCREMENT voor een tabel `tblbehandeling`
 --
-ALTER TABLE `tblKlant`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
+ALTER TABLE `tblbehandeling`
+  MODIFY `BehandelingID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT voor een tabel `tblPersoneel`
+-- AUTO_INCREMENT voor een tabel `tblklant`
 --
-ALTER TABLE `tblPersoneel`
-  MODIFY `KapsterID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `tblklant`
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT voor een tabel `tblpersoneel`
+--
+ALTER TABLE `tblpersoneel`
+  MODIFY `KapsterID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `tblafspraak`
+--
+ALTER TABLE `tblafspraak`
+  ADD CONSTRAINT `tblafspraak_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tblklant` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tblafspraak_ibfk_2` FOREIGN KEY (`BehandelingID`) REFERENCES `tblbehandeling` (`BehandelingID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tblafspraak_ibfk_3` FOREIGN KEY (`KapsterID`) REFERENCES `tblpersoneel` (`KapsterID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `tblklant`
+--
+ALTER TABLE `tblklant`
+  ADD CONSTRAINT `tblklant_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `login` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

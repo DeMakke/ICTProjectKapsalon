e7590c1d08c259c51dac6748bfb91ecb42d5407c
janyoulton <j.youlton@gmail.com>
2016-05-16 15:14:39 +0200

    uitbreiding let 2de tabel
    test eerst bij jezelf of je een afspraak tegoei in je database wegschrijft, enkel de datum/tijd klopt nog niet (database 'Tijd' moet datetime zijn!)
    Het 'tweede deel' (=tabel met de klant zn bestaande afspraken tonen) geeft nog een error registreren dat gebruiker al bestaat? Al deze stukken in controller/view heb ik in commentaar gezet.

--- application/controllers/AfspraakUser.php
+++ application/controllers/AfspraakUser.php
@@ -16,43 +16,47 @@ class AfspraakUser extends CI_Controller{
         
         }
         $this->load->database();
-        $this->load->model('afspraakUser_model');
+        $this->load->model('afspraak_model');
         $this->load->model('personeel_model');
-        
     }
     
     public function index(){
         $id = $this->uri->segment(3);
         
-        $data['behandelingen'] = $this->afspraakUser_model->toonBehandeling($id)->result();
+//        $id = $this->session->userdata('usersessie')['id_user'];
+        
+        $data['behandelingen'] = $this->afspraak_model->toonBehandeling()->result();
         $data['personeel'] = $this->personeel_model->get_all($id)->result();
+        
+        //alle data in commentaar is om de bestaande afspraken in de 2de tabel te tonen
+        
+//        $data['afspraakKlant'] = $this->afspraak_model->getKlant($id)->result();
+//        $data['afspraakBehandeling'] = $this->afspraak_model->getBehandeling($id)->result();
+//        $data['afspraakTijd'] = $this->afspraak_model->getTijd($id)->result();
+//        $data['afspraakKapster'] = $this->afspraak_model->getKapster($id)->result();
+//        $data['aantal'] = $this->afspraak_model->getAantalAfspraken($id)->result();
         
         if (isset($_POST['btn-afspraak'])){
             
             $dag = $_POST['dagSelect'];
-            $uur = $_POST['tijdSelect'];
+            $uur = $_POST['uurSelect'];
         
-            $dateArray = getdate($uur);  
-            $dateArray['hours'];
-            $dateArray['minutes'];  
-        
-            $tijdstip = date("Y-m-d H:i:s",mktime($dateArray['hours'],$dateArray['minutes'], 0, 0,$dag));
+            $dateArray = explode(":",$uur); //uur en minuten opsplitsen
+            
+            $tijdstip = mktime(intval($dateArray['0']),intval($dateArray['1']), 0, 0,intval($dag));
+            //$tijdstip = mktime(11, 14, 54, 8, 12, 2014); //dit is een test om de werking van de tijd te tonen
             
             $afspraak = array(
             'BehandelingID' => $_POST['typeSelect'],
             'KapsterID' => $_POST['persSelect'],
+            //'id_user' => $id, -> variabele bovenaan index() omdat id nu ook in model gebruikt zal worden
             'id_user' => $this->session->userdata('usersessie')['id_user'],
-            'Tijd' => $tijdstip,
+            'Tijd' => date("Y-m-d H:i:s",$tijdstip),
             );
-            $this->afspraakUser_model->insertAfspraak($afspraak);
-            redirect('homeUser');
+            $this->afspraak_model->insertAfspraak($afspraak);
+            redirect('afspraakUser');
         }else{
             $this->load->view('afspraakUser',$data);
         }
-        
     }
-    
-
-    
-
 }

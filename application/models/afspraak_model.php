32bf6ad7ba400e5f6dd2aedb6fa0dc408adbaaa1
janyoulton <j.youlton@gmail.com>
2016-05-19 15:02:17 +0200

    Add files via upload

--- application/models/afspraak_model.php
+++ application/models/afspraak_model.php
@@ -20,44 +20,46 @@ class afspraak_model extends CI_Model
         return $this->db->insert('tblafspraak',$data);
     }
     
-    public function getKlant($id){
-        $this->db->select('tblklant.username');
-        $this->db->from('tblklant');
-        $this->db->join('tblafspraak', 'tblklant.id_user = tblafspraak.id_user','inner');
-        $this->db->where('id_user',$id);
+    public function getAfspraak($id){
+        $this->db->select('tblafspraak.AfspraakDag,tblklant.username, tblafspraak.Tijd,tblbehandeling.Type, tblpersoneel.Voornaam');
+        $this->db->from('tblafspraak');
+        $this->db->join('tblklant', 'tblklant.id_user = tblafspraak.id_user');
+        $this->db->join('tblbehandeling','tblafspraak.BehandelingID = tblbehandeling.BehandelingID');
+        $this->db->join('tblpersoneel','tblafspraak.KapsterID = tblpersoneel.KapsterID');
+        $this->db->where('tblafspraak.id_user',$id);
         return $this->db->get();
     }
     
-    public function getBehandeling($id){
-        $this->db->select('tblbehandeling.Type');
-        $this->db->from('tblbehandeling');
-        $this->db->join('tblafspraak', 'tblbehandeling.BehandelingID = tblafspraak.BehandelingID');
-        $this->db->where('id_user',$id);
-        $behandeling = $this->db->get();
-        return $behandeling;
-    }
-    
-    public function getTijd($id){
-        $this->db->select('Datum');
-        $this->db->from('tblafspraak');
-        $this->db->where('id_user',$id);
-        $tijd = $this->db->get();
-        return $tijd;
-    }
-    
-    public function getKapster($id){
-        $this->db->select('voornaam');
-        $this->db->from('tblpersoneel');
-        $this->db->join('tblafspraak', 'tblpersoneel.KapsterID = tblafspraak.KapsterID');
-        $this->db->where('id_user',$id);
-        $kapster = $this->db->get();
-        return $kapster;
-    }
-    
-    public function getAantalAfspraken($id){
-        $this->db->select('*');
-        $this->db->from('tblafspraak');
-        $this->db->where('id_user',$id);
-        return $this->db->count_all_results();
-    }
+//    public function getBehandeling($id){
+//        $this->db->select('tblbehandeling.Type');
+//        $this->db->from('tblbehandeling');
+//        $this->db->join('tblafspraak', 'tblbehandeling.BehandelingID = tblafspraak.BehandelingID');
+//        $this->db->where('id_user',$id);
+//        $behandeling = $this->db->get();
+//        return $behandeling;
+//    }
+//    
+//    public function getTijd($id){
+//        $this->db->select('Datum');
+//        $this->db->from('tblafspraak');
+//        $this->db->where('id_user',$id);
+//        $tijd = $this->db->get();
+//        return $tijd;
+//    }
+//    
+//    public function getKapster($id){
+//        $this->db->select('voornaam');
+//        $this->db->from('tblpersoneel');
+//        $this->db->join('tblafspraak', 'tblpersoneel.KapsterID = tblafspraak.KapsterID');
+//        $this->db->where('id_user',$id);
+//        $kapster = $this->db->get();
+//        return $kapster;
+//    }
+//    
+//    public function getAantalAfspraken($id){
+//        $this->db->select('*');
+//        $this->db->from('tblafspraak');
+//        $this->db->where('id_user',$id);
+//        return $this->db->count_all_results();
+//    }
 }

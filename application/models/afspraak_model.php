f318e81b7bf5d23b0d9563fdd4e6d919d292485d
janyoulton <j.youlton@gmail.com>
2016-05-20 13:52:03 +0200

    afspraak model
    select query moet specifiek zijn aangezien sommige veldnamen in 2 verschillende tabellen dezelfde namen delen.

--- application/models/afspraak_model.php
+++ application/models/afspraak_model.php
@@ -12,16 +12,18 @@ class afspraak_model extends CI_Model
         $this->load->database();
     }
     
+    //toon behandelingen voor formulier selectType
     public function toonBehandeling(){
         return $this->db->get_where('tblbehandeling');
     }
-    
+    //de gekozen waardes wegschrijven naar de tabel afspraak
     public function insertAfspraak($data){
         return $this->db->insert('tblafspraak',$data);
     }
-    
+    //alle afspraken van huidige user tonen met benodigde details
     public function getAfspraak($id){
-        $this->db->select('tblafspraak.AfspraakDag,tblklant.username, tblafspraak.Tijd,tblbehandeling.Type, tblpersoneel.Voornaam');
+        $this->db->select('tblafspraak.AfspraakDag, tblafspraak.Tijd,tblbehandeling.Type, '
+                . 'tblpersoneel.Voornaam, tblpersoneel.Achternaam');
         $this->db->from('tblafspraak');
         $this->db->join('tblklant', 'tblklant.id_user = tblafspraak.id_user');
         $this->db->join('tblbehandeling','tblafspraak.BehandelingID = tblbehandeling.BehandelingID');
@@ -30,9 +32,11 @@ class afspraak_model extends CI_Model
         $this->db->order_by('tblafspraak.AfspraakDag, tblafspraak.Tijd','asc');
         return $this->db->get();
     }
-    
-    public function toonAfspraakAdmin(){
-        $this->db->select('tblafspraak.AfspraakDag,tblklant.username, tblafspraak.Tijd,tblbehandeling.Type, tblpersoneel.Voornaam');
+    //alle afspraken van alle users tonen met benodigde details
+    public function getAlleAfspraken(){
+        $this->db->select('tblafspraak.afspraakID,tblklant.username, tblklant.Voornaam AS klantVoornaam, tblklant.Achternaam AS klantAchternaam, '
+                . 'tblafspraak.AfspraakDag,tblafspraak.Tijd, tblbehandeling.Type, tblbehandeling.Prijs,'
+                . ' tblpersoneel.Voornaam as persVoornaam, tblpersoneel.Achternaam AS persAchternaam');
         $this->db->from('tblafspraak');
         $this->db->join('tblklant', 'tblklant.id_user = tblafspraak.id_user');
         $this->db->join('tblbehandeling','tblafspraak.BehandelingID = tblbehandeling.BehandelingID');
@@ -41,36 +45,9 @@ class afspraak_model extends CI_Model
         return $this->db->get();
     }
     
-//    public function getBehandeling($id){
-//        $this->db->select('tblbehandeling.Type');
-//        $this->db->from('tblbehandeling');
-//        $this->db->join('tblafspraak', 'tblbehandeling.BehandelingID = tblafspraak.BehandelingID');
-//        $this->db->where('id_user',$id);
-//        $behandeling = $this->db->get();
-//        return $behandeling;
-//    }
-//    
-//    public function getTijd($id){
-//        $this->db->select('Datum');
-//        $this->db->from('tblafspraak');
-//        $this->db->where('id_user',$id);
-//        $tijd = $this->db->get();
-//        return $tijd;
-//    }
-//    
-//    public function getKapster($id){
-//        $this->db->select('voornaam');
-//        $this->db->from('tblpersoneel');
-//        $this->db->join('tblafspraak', 'tblpersoneel.KapsterID = tblafspraak.KapsterID');
-//        $this->db->where('id_user',$id);
-//        $kapster = $this->db->get();
-//        return $kapster;
-//    }
-//    
-//    public function getAantalAfspraken($id){
-//        $this->db->select('*');
-//        $this->db->from('tblafspraak');
-//        $this->db->where('id_user',$id);
-//        return $this->db->count_all_results();
-//    }
+    public function row_delete($id){
+        //$id=$_GET['id'];
+        $this->db->where('afspraakID',$id);
+        $this->db->delete('tblafspraak');
+    }
 }
